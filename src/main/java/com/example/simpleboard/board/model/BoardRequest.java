931012fe631dd9c1ee43c04416974aba79174f5f
k9want <zndwlrnjr26@daum.net>
2024-02-26 20:27:00 +0900

    Add: [post] /api/board - 게시판 생성

---
+++ src/main/java/com/example/simpleboard/board/model/BoardRequest.java
@@ -0,0 +1,24 @@
+package com.example.simpleboard.board.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BoardRequest {
+
+    @NotBlank
+    private String boardName;
+}
---
+++ src/main/java/com/example/simpleboard/board/service/BoardService.java
@@ -0,0 +1,23 @@
+package com.example.simpleboard.board.service;
+
+import com.example.simpleboard.board.db.BoardEntity;
+import com.example.simpleboard.board.db.BoardRepository;
+import com.example.simpleboard.board.model.BoardRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardEntity create(BoardRequest boardRequest) {
+        BoardEntity entity = BoardEntity.builder()
+                .boardName(boardRequest.getBoardName())
+                .status("REGISTERED")
+                .build();
+
+        return boardRepository.save(entity);
+    }
+}
